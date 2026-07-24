f4463c7d5ebb61085aa9e0c7cc9c36d972e86ad0
efeerenakar0 <84183940+efeerenakar0@users.noreply.github.com>
2026-07-24 07:01:54 +0300

    feat: redirect homepage (/) directly to AI Fabrikası (/fabrika)

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,23 +1,5 @@
-import Hero from "@/components/home/Hero";
-import Stats from "@/components/home/Stats";
-import FeaturedProjects from "@/components/home/FeaturedProjects";
-import ServicesSummary from "@/components/home/ServicesSummary";
-import WhyJasmine from "@/components/home/WhyJasmine";
-import PartnerCTA from "@/components/home/PartnerCTA";
-import Testimonials from "@/components/home/Testimonials";
-import ContactPreview from "@/components/home/ContactPreview";
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Stats />
-      <FeaturedProjects />
-      <ServicesSummary />
-      <WhyJasmine />
-      <PartnerCTA />
-      <Testimonials />
-      <ContactPreview />
-    </>
-  );
+  redirect('/fabrika');
 }
